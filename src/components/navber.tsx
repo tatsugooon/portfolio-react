14abecb4197327b2ea69322a6a5bfe1548f790c6
Tatsuya Yoda <yoda@limar.co.jp>
2023-03-03 22:02:52 +0900

    スムーススクロール

--- src/components/navber.tsx
+++ src/components/navber.tsx
@@ -1,22 +1,23 @@
+import { Link as Scroll } from 'react-scroll'
 
 const Navbar = () => {
     return (
         <div className='navbar fixed z-10 text-left p-6 font-oswald'>
-            <a href='/#about' className='p-2 underline underline-offset-2 hover:underline-offset-8 mr-2'>
+            <Scroll to="about" smooth={true} offset={-50} className='p-2 underline underline-offset-2 hover:underline-offset-8 mr-2'>
                 About.
-            </a>
-            <a href='/#works' className='p-2 underline underline-offset-2 hover:underline-offset-8 mr-2'>
+            </Scroll>
+            <Scroll to="works" smooth={true} offset={-50} className='p-2 underline underline-offset-2 hover:underline-offset-8 mr-2'>
                 Works.
-            </a>
-            <a href='/#video' className='p-2 underline underline-offset-2 hover:underline-offset-8 mr-2'>
+            </Scroll>
+            <Scroll to="video" smooth={true} offset={-50} className='p-2 underline underline-offset-2 hover:underline-offset-8 mr-2'>
                 Video.
-            </a>
-            <a href='/#experiences' className='p-2 underline underline-offset-2 hover:underline-offset-8 mr-2'>
+            </Scroll>
+            <Scroll to="experiences" smooth={true} offset={-50} className='p-2 underline underline-offset-2 hover:underline-offset-8 mr-2'>
                 Experiences.
-            </a>
-            <a href='/#contact' className='p-2 underline underline-offset-2 hover:underline-offset-8'>
+            </Scroll>
+            <Scroll to="contact" smooth={true} offset={-50} className='p-2 underline underline-offset-2 hover:underline-offset-8 mr-2'>
                 Contact.
-            </a>
+            </Scroll>
         </div>
     )
 }
